fc3ec86a20e637500df33e1e3a73add76e1b8db8
ITakayamaR <itakayama@diacsa.com>
2021-04-27 16:13:27 -0500

    v1.3

--- sofnet_script.py
+++ sofnet_script.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #Exportamos el modulo subprocess
-from subprocess import call
 import serial
 import time
+from subprocess import call
+import netifaces
 
 ## Preambulo de la data en emisión/recepción ($SOF)
 pream=bytes([36,83,79,70])
+
 ## Puerto usb a conectar 
-puerto = "/dev/ttyUSB0"
+puerto = "/dev/ttyUSB1"
 
 #######################################
 ## Funcion que calcula el CRC16
@@ -65,7 +67,7 @@ def ReceiveCmd():
             print("Error: "+ str(pream) + " no concuerda")
             return [-1]
         if(int(trama[4])!=len(trama)-5):
-            print("Error: longitud de trama no concuerda")
+            #print("Error: longitud de trama no concuerda")
             continue
         if(trama[5]!= 65 and trama[5]!= 73 ):
             print("Error: No se reconoce el comando")
@@ -77,76 +79,63 @@ def ReceiveCmd():
         if(crc != bytes(trama[-2:])):
             print(crc)
             print("Error: CRC no concuerda")
-            return [-1]
+            #return [-1]
         msg = trama[5:len(trama)-2]
-
-        #Responde la comunicación
-        data = bytes([3,10])
-        crc = CRC16(data)
-        MDP = pream
-        trama = MDP + data + crc
-
-        serial_port.write(trama)
-        print("Responde la trama")
+        
         print(trama)
         return msg
 
     return [-2]   # Error timeout
 
-def sendCmd(cmd,data=None,data2=None):
-    if(cmd>255):
-        print("No existe el comando")
-        return 1
-    cmd = cmd.to_bytes(1,'big')
-    if (data==0):
-      data_size = 1
-      data = data.to_bytes(data_size,'big')
-    else:
-        if(data==None):
-            data_size = 0
-            data = bytearray(0)
-        else:
-            data_size = (data.bit_length()+7)//8
-            data = data.to_bytes(data_size,'big')
-    if(data2==None):
-        data2_size = 0
-        data2 = bytearray(0)
-    else:
-        data2_size = (data2.bit_length()+7)//8
-        data2 = data2.to_bytes(data2_size,'big')
-    #print(data_size)
-
-    MDP = pream
-    longitud = bytes([3+data_size+data2_size])
-    trama = longitud + cmd + data + data2
-    crc = crc16(trama)
-    trama = MDP + trama + crc
-    serial_port.write(trama)
-
-
 print("Start program")
 
 serial_port = ConfigSerial()
 
+lista=[]
 while (1):
     i=ReceiveCmd()
-    if (i):
-        print(i[0])
+    if (i[0] > 0):
         if (i[0]==65):
-            print("u")
-            print(["sudo","shutdown"])
+            print("La maquina se apagará")
+            call(["sudo","shutdown"])
+            break
+        
         elif (i[0]==73):
-            ip 	= '.'.join(i[1:4])
-            gw 	= '.'.join(i[5:8])
-            nm 	= '.'.join(i[9:12])
-            dns	= '.'.join(i[13:16])
-#         i = int(ser.readline().decode('utf-8').rstrip())
-#         #call(["sudo","service","network, "stop"])
-            print(["sudo","route", "add", "default","gw",gw])
             
-            print(["sudo", "ifconfig", "eth0", ip, "netmask", nm])
-#         #(["sudo", "ifconfig", "wlan0", "192.168.1.6", "netmask", "255.255.255.0"])
+            #Transformamos la lista de byte a string
+            for n in i:
+                lista.append(str(n))
+            
+            #Filtramos las interfaces correspondientes a las iniciales "enp"
+            interface_list = netifaces.interfaces()
+            interface_filter=[k for k in interface_list if 'enp' in k]
+            
+            print("Las interfaces detectadas son: ")
+            print(interface_filter)
+            
+            #call(["sudo","systemctl","restart","NetworkManager"])
+            
+            #Separamos el ip, gw, netmask y dns para cada interfaz
+            for index,interface in enumerate(interface_filter):
+                ip 	= '.'.join(lista[1+(16*index):5+(16*index)])
+                gw 	= '.'.join(lista[5+(16*index):9+(16*index)])
+                nm 	= '.'.join(lista[9+(16*index):13+(16*index)])
+                dns	= '.'.join(lista[13+(16*index):17+(16*index)])
 
-# 
-#         call(["sudo","ifconfig", confg[i]["interfaz"], "up"])
+                #Eliminamos el DNS por default
+                call(["nmcli", "device", "mod", interface, "ipv4.ignore-auto-dns", "yes"])
+                #Agregamos el DNS correspondiente
+                call(["nmcli", "device", "mod", interface, "ipv4.dns", dns])
+                
+                #reiniciamos la interfaz para que el cambio haga efecto
+                call(["sudo","ifconfig", interface, "down"])
+                call(["sudo","ifconfig", interface, "up"])
+                
+                #Cambiamos la ip y la mascara de red
+                call(["sudo", "ifconfig", interface, ip, "netmask", nm])
+                #Cambiamos el gateway
+                call(["sudo","route", "add", "default","gw",gw, interface])
+                
+            #call(["sudo","systemctl","restart","NetworkManager"])
+                
     time.sleep(1)
